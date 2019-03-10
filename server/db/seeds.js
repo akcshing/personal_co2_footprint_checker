6e2c3020ff97ad27cd503dd4c70cec6b88c7f69c
awmcf <aaronmcfaull@gmail.com>
2019-03-10 15:04:11 +0000

    Added seed data with country's footprint per capita

---
+++ server/db/seeds.js
@@ -0,0 +1,62 @@
+use co2footprint;
+db.dropDatabase;
+
+db.userFootprints.insertMany(
+[
+{
+ diet: "Meat-eater",
+ commute: "Car",
+ recycling: "Sometimes",
+ footprint: 4
+}
+]
+);
+
+db.countryFootprints.insertMany(
+ [
+   {
+     country: "Qatar",
+     footprintPerCapita: 9
+   },
+
+   {
+     country: "United Kingdom",
+     footprintPerCapita: 8
+   },
+
+   {
+     country: "Hong Kong",
+     footprintPerCapita: 8
+   },
+
+   {
+     country: "Spain",
+     footprintPerCapita: 7
+   },
+
+   {
+     country: "Hungary",
+     footprintPerCapita: 7
+   },
+
+   {
+     country: "Romania",
+     footprintPerCapita: 6
+   },
+
+   {
+     country: "Pakistan",
+     footprintPerCapita: 5
+   },
+
+   {
+     country: "Cambodia",
+     footprintPerCapita: 4
+   },
+
+   {
+     country: "Ethiopia",
+     footprintPerCapita: 3
+   },
+ ]
+);
