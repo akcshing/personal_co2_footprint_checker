8a5f99c1e36e45532fa95fac34d54cf589ee802d
Alexkcshing <alexkcshing@gmail.com>
2019-03-13 14:37:38 +0000

    Timeline done and fixed

--- client/src/views/cool_data_view.js
+++ client/src/views/cool_data_view.js
@@ -53,137 +53,190 @@ CoolDataView.prototype.renderChart = function (footprint) {
     }
     seriesArr.push(seriesObj)
   });
-    Highcharts.chart(this.container, {
-      chart: {
-          type: 'column'
-      },
+  Highcharts.chart(this.container, {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: "Footprint Result"
+    },
+    xAxis: {
+      categories: ['Your footprint result']
+    },
+    yAxis: {
+      min: 0,
       title: {
-          text: "Footprint Result"
+        text: "Footprint Scores"
       },
-      xAxis: {
-          categories: ['Your footprint result']
-      },
-      yAxis: {
-          min: 0,
-          title: {
-              text: "Footprint Scores"
-          },
-          stackLabels: {
-              enabled: true,
-              style: {
-                  fontWeight: 'bold',
-                  color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-              }
-          }
-      },
-      legend: {
-          align: 'right',
-          x: -30,
-          verticalAlign: 'top',
-          y: 25,
-          floating: true,
-          backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-          borderColor: '#CCC',
-          borderWidth: 1,
-          shadow: false
-      },
-      tooltip: {
-          headerFormat: '<b>{point.x}</b><br/>',
-          pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-      },
-      plotOptions: {
-          column: {
-              stacking: 'normal',
-              dataLabels: {
-                  enabled: true,
-                  color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
-              }
-          }
-      },
-      series: seriesArr
+      stackLabels: {
+        enabled: true,
+        style: {
+          fontWeight: 'bold',
+          color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+        }
+      }
+    },
+    legend: {
+      align: 'right',
+      x: -30,
+      verticalAlign: 'top',
+      y: 25,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+      borderColor: '#CCC',
+      borderWidth: 1,
+      shadow: false
+    },
+    tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true,
+          color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+        }
+      }
+    },
+    series: seriesArr
 
   });
 };
 
-  CoolDataView.prototype.renderTimelineChart = function (sortedFootprints) {
+CoolDataView.prototype.renderTimelineChart = function (sortedFootprints) {
 
 
-      // const seriesArr =[]
-      // sortedFootprints.forEach((footprint) => {
-      //   seriesObj = {
-      //     name: footprint.footprint,
-      //     data: [1]
-      //   }
-      //   seriesArr.push(seriesObj)
-      // });
+  // const seriesArr =[]
+  // sortedFootprints.forEach((footprint) => {
+  //   seriesObj = {
+  //     name: footprint.footprint,
+  //     data: [1]
+  //   }
+  //   seriesArr.push(seriesObj)
+  // });
 
-      const footprintArr = [];
-      sortedFootprints.forEach((footprint) => {
-        footprintArr.push(footprint.footprint);
-      })
+  const footprintArr = [];
+  sortedFootprints.forEach((footprint) => {
+    footprintArr.push(footprint.footprint);
+  })
 
-      const datesArr = [];
-      sortedFootprints.forEach((footprint) => {
-        datesArr.push(footprint.date);
-      });
+  const datesArr = [];
+  sortedFootprints.forEach((footprint) => {
+    datesArr.push(footprint.date);
+  });
 
-      console.log("datesarray:", datesArr);
+  const dietDataArr = [];
+  const commuteDataArr = [];
+  const recyclingDataArr = [];
+  const heatingDataArr = [];
+  const flyingDataArr = [];
+  const clothesDataArr = [];
+  const electronicsDataArr = [];
+  const petsDataArr = [];
+  const beautyDataArr = [];
+  const paperDataArr = [];
 
-        Highcharts.chart(this.container, {
-          chart: {
-              type: 'column'
-          },
-          title: {
-              text: "Footprint Result"
-          },
-          xAxis: {
-              categories: datesArr
-          },
-          yAxis: {
-              min: 0,
-              title: {
-                  text: "Footprint Scores"
-              },
-              stackLabels: {
-                  enabled: true,
-                  style: {
-                      fontWeight: 'bold',
-                      color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-                  }
-              }
-          },
-          legend: {
-              align: 'right',
-              x: -30,
-              verticalAlign: 'top',
-              y: 25,
-              floating: true,
-              backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-              borderColor: '#CCC',
-              borderWidth: 1,
-              shadow: false
-          },
-          tooltip: {
-              headerFormat: '<b>{point.x}</b><br/>',
-              pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-          },
-          plotOptions: {
-              column: {
-                  stacking: 'normal',
-                  dataLabels: {
-                      enabled: true,
-                      color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
-                  }
-              }
-          },
-          series: [{
-            name: 'Footprint',
-            data: footprintArr
-          }]
+  sortedFootprints.forEach((footprint) => {
+    dietDataArr.push(parseInt(Object.values(footprint.scores[0])));
+    commuteDataArr.push(parseInt(Object.values(footprint.scores[1])));
+    recyclingDataArr.push(parseInt(Object.values(footprint.scores[2])));
+    heatingDataArr.push(parseInt(Object.values(footprint.scores[3])));
+    flyingDataArr.push(parseInt(Object.values(footprint.scores[4])));
+    clothesDataArr.push(parseInt(Object.values(footprint.scores[5])));
+    electronicsDataArr.push(parseInt(Object.values(footprint.scores[6])));
+    petsDataArr.push(parseInt(Object.values(footprint.scores[7])));
+    beautyDataArr.push(parseInt(Object.values(footprint.scores[8])));
+    paperDataArr.push(parseInt(Object.values(footprint.scores[9])));
+  });
 
-        });
+  console.log("I AM DIET ARRAY:", paperDataArr);
 
-      };
+  // console.log("datesarray:", datesArr);
+
+  Highcharts.chart(this.container, {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: "Footprint Result"
+    },
+    xAxis: {
+      categories: datesArr
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: "Footprint Scores"
+      },
+      stackLabels: {
+        enabled: true,
+        style: {
+          fontWeight: 'bold',
+          color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+        }
+      }
+    },
+    legend: {
+      align: 'right',
+      x: -30,
+      verticalAlign: 'top',
+      y: 25,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+      borderColor: '#CCC',
+      borderWidth: 1,
+      shadow: false
+    },
+    tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true,
+          color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+        }
+      }
+    },
+    series: [{
+      name: 'Diet',
+      data: dietDataArr
+    }, {
+      name: 'Commute',
+      data: commuteDataArr
+    }, {
+      name: 'Recycling',
+      data: recyclingDataArr
+    }, {
+      name: 'Heating',
+      data: heatingDataArr
+    }, {
+      name: 'Flying',
+      data: flyingDataArr
+    }, {
+      name: 'Clothes',
+      data: clothesDataArr
+    }, {
+      name: 'Electronics',
+      data: electronicsDataArr
+    }, {
+      name: 'Pets',
+      data: petsDataArr
+    }, {
+      name: 'Beauty',
+      data: beautyDataArr
+    }, {
+      name: 'Paper',
+      data: paperDataArr
+    }]
+
+  });
+
+};
 
 
-  module.exports = CoolDataView;
+module.exports = CoolDataView;
